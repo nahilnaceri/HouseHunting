aa4cdfe094c2197c1b2c59bf13768b036e6c45d5
nahilnaceri <chafikmihoubnaceri@gmail.com>
2020-09-27 21:44:09 +0100

    months to downpayment script

---
+++ calculate.py
@@ -0,0 +1,21 @@
+
+# cost of dream house
+totalCost = float(input("Cost of house "))
+# percent downpayemnt
+portion_down_payment= 0.25
+
+currentSavings = 0.0
+ # function for each month investement gain
+ #  current_savings*r/12
+r = 0.04 # percentage investement gain
+annualSalary = float(input("Your Annual Salary: "))
+portionSaved = float(input("Portion Saved ")) # protion saved for downpayement from salary
+
+monthlySaved = portionSaved * annualSalary / 12
+numberOfMonths = 0
+
+# loop every month and increase the savings once the savings is = or > than stop
+while currentSavings < portion_down_payment * totalCost:
+    currentSavings += monthlySaved + (currentSavings*r/12)
+    numberOfMonths+=1
+print(numberOfMonths)
